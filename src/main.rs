adc1b71831f4656ca1802d959f3b728020133632
Douglas Poma <douglaspoma@yahoo.com>
2024-02-06 09:45:57 -0300

    feat: initial verification

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Guessing Game");
+    loop {
+        println!("Bem vindo ao jogo da advinhação!");
+        println!("i - Iniciar o jogo");
+        println!("q - Fechar o jogo");
+
+        let mut escolha_str = String::new();
+        let _ = stdin().read_line(&mut escolha_str);
+        match escolha_str.trim().to_lowercase().as_str() {
+            "i" => {
+                println!("Iniciando o jogo!");
+            }
+            "q" => {
+                println!("Obrigado por jogar!");
+                break;
+            }
+            _ => {
+                println!("Opção inválida! Tente novamente.");
+                continue;
+            }
+        };
+    }
 }
